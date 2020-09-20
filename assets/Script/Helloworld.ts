e092f1b8fde3c7a72b4c57aebecf4068bdd226be
wayne <zglw2012@163.com>
2020-09-20 23:24:40 +0800

    init

---
+++ assets/Script/Helloworld.ts
@@ -0,0 +1,20 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class Helloworld extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello,world!!!';
+
+    onLoad(){
+
+    }
+
+    start() {
+        // init logic
+    }
+
+}
---
+++ assets/Script/UIFrame/UIBase.ts
@@ -0,0 +1,8 @@
+/**
+ * @ Author: liuwei
+ * @ Create Time: 2020-09-20 16:43:16
+ * @ Modified by: liuwei
+ * @ Modified time: 2020-09-20 16:43:24
+ * @ Description: 基础类
+ */
+
